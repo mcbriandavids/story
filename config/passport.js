b989df5729bc99890ab7de485b4269dc06a34a38
Imonisa Oghenekevwe Brian <42264681+mcbriandavids@users.noreply.github.com>
2019-12-03 13:33:02 +0100

    https

--- config/passport.js
+++ config/passport.js
@@ -3,7 +3,7 @@ const config = require('config')
 module.exports =function(passport){passport.use(new GoogleStrategy({
     clientID: config.get('googleClientID'),
     clientSecret: config.get('googleClientSecret'),
-    callbackURL: "http://https://storybooks2019.herokuapp.com/auth/google/callback"
+    callbackURL: "https://storybooks2019.herokuapp.com/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
  console.log(accessToken)
